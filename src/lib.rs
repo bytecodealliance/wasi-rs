ece604756ebd5f6a8424a4e1b4cf170cd59a3d95
Dan Gohman <sunfish@mozilla.com>
2019-07-22 06:55:36 -0700

    Clippy fixes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,16 @@
+#![cfg_attr(
+    feature = "cargo-clippy",
+    warn(
+        clippy::float_arithmetic,
+        clippy::mut_mut,
+        clippy::nonminimal_bool,
+        clippy::option_map_unwrap_or,
+        clippy::option_map_unwrap_or_else,
+        clippy::print_stdout,
+        clippy::unicode_not_nfc,
+        clippy::use_self
+    )
+)]
+#![no_std]
+
 pub mod wasi_unstable;
