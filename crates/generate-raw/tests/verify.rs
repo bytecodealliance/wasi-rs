e5bfbde690fafe9b22d6972a2634ed044b170da9
Josh Triplett <josh@joshtriplett.org>
2019-12-01 00:55:40 -0800

    Separate generated code into a private submodule lib_generated
    Move all the generated code into `lib_generated.rs`, load that as a
    private submodule, and `pub use lib_generated::*;`. This allows moving
    the crate documentation, `#![no_std]`, and `mod error;` out of the
    generated code and into the now-non-generated `lib.rs`. This makes the
    generated code capable of functioning with `include!` in the future.
    
    Modify the generated `strerror` to use `pub(crate)` so that the `error`
    module can reference it.
    
    These changes have no effect on the visible API or documentation of the
    crate.

--- crates/generate-raw/tests/verify.rs
+++ crates/generate-raw/tests/verify.rs
@@ -1,6 +1,6 @@
 #[test]
 fn assert_same_as_src() {
-    let actual = include_str!("../../../src/lib.rs");
+    let actual = include_str!("../../../src/lib_generated.rs");
     let expected =
         generate_raw::generate("WASI/phases/snapshot/witx/wasi_snapshot_preview1.witx".as_ref());
     if actual == expected {
@@ -12,7 +12,7 @@ fn assert_same_as_src() {
 the generate `raw.rs` does not match the actual source `raw.rs`, it's
 recommended to run this command from the root of the repository:
 
-    cargo run -p generate-raw crates/generate-raw/WASI > src/lib.rs
+    cargo run -p generate-raw crates/generate-raw/WASI > src/lib_generated.rs
 
 "
     );
