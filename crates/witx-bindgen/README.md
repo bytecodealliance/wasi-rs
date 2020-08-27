24dae9480a662a4c226ddf3b5f735bae94380c96
Andrew Brown <andrew.brown@intel.com>
2020-08-27 10:33:45 -0700

    Allow generate-raw to run single witx files (#45)
    * Make witx-bindgen run directly on witx files
    
    * Document how to use witx-bindgen
    
    * Update failed `assert_same_as_src` test instructions

---
+++ crates/witx-bindgen/README.md
@@ -0,0 +1,9 @@
+# witx-bindgen
+
+Generate Rust code matching a witx interface.
+
+### Use
+
+```
+cargo run [path to witx file]
+```
--- crates/witx-bindgen/tests/verify.rs
+++ crates/witx-bindgen/tests/verify.rs
@@ -1,19 +1,20 @@
 #[test]
 fn assert_same_as_src() {
     let actual = include_str!("../../../src/lib_generated.rs");
-    let expected =
-        witx_bindgen::generate(&["WASI/phases/snapshot/witx/wasi_snapshot_preview1.witx"]);
+    let witx_path = "WASI/phases/snapshot/witx/wasi_snapshot_preview1.witx";
+    let expected = witx_bindgen::generate(&[witx_path]);
     if actual == expected {
         return;
     }
     panic!(
         "
 
-the generate `raw.rs` does not match the actual source `raw.rs`, it's
+the generated `raw.rs` does not match the actual source `raw.rs`, it's
 recommended to run this command from the root of the repository:
 
-    cargo run -p witx-bindgen crates/witx-bindgen/WASI > src/lib_generated.rs
+    cargo run -p witx-bindgen crates/witx-bindgen/{} > src/lib_generated.rs
 
-"
+",
+        witx_path
     );
 }
