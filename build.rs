a8917786b0006baf3b09dee48018e4736a2bb9bc
Eduardo Rodrigues <eduardomourar@users.noreply.github.com>
2023-04-27 00:54:38 +0200

    chore: move build script to root

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() -> anyhow::Result<()> {
-    wit_deps::lock_sync!()?;
+    wit_deps::lock_sync!("crates/wasi-http/wit")?;
 
     println!("cargo:rerun-if-changed=wit/deps");
     println!("cargo:rerun-if-changed=wit/deps.lock");
