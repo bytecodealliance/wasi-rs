dae8f0c913672b7fc0f5ecf22b70bae9dd2801e8
Alex Crichton <alex@alexcrichton.com>
2024-01-30 06:53:00 -0800

    Update to WASI 0.2.0 bindings

---
+++ ci/regenerate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+wit-bindgen rust wit --out-dir src --std-feature
+
+# rustfmt chokes on the raw output of wit-bindgen right now due to trailling
+# whitespace (unsure as to why), so format it with some options first to get it
+# into a better state before applying the final format with default options
+# which gets this to succeed.
+#
+# NB: this should be considered a bug in wit-bindgen that this is required to do
+# twice. Passing `--rustfmt` to `wit-bindgen` should work.
+rustfmt src/bindings.rs --edition 2021 --config-path ./ci/rustfmt-bindings.toml
+rustfmt src/bindings.rs --edition 2021
