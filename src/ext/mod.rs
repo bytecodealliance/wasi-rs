03e63a68e1f194b369f2b04ef9e467d1ed6cc229
Roman Volosatovs <rvolosatovs@riseup.net>
2024-05-13 13:08:36 +0200

    feat(io): implement `Read` and `Write` for streams
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- src/ext/mod.rs
+++ src/ext/mod.rs
@@ -1,8 +1,8 @@
+#[cfg(feature = "std")]
+mod std;
+
 impl core::fmt::Display for crate::io::error::Error {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         f.write_str(&self.to_debug_string())
     }
 }
-
-#[cfg(feature = "std")]
-impl std::error::Error for crate::io::error::Error {}
