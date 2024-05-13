9825655f7ddb236a2984e27ec92d6699f00956bc
Roman Volosatovs <rvolosatovs@riseup.net>
2024-05-13 13:33:21 +0200

    feat(rand): add `rand` crate integration
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- src/ext/mod.rs
+++ src/ext/mod.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "std")]
 mod std;
 
+#[cfg(feature = "rand")]
+pub mod rand;
+
 impl core::fmt::Display for crate::io::error::Error {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         f.write_str(&self.to_debug_string())
